a6d44e0fb83fc95d3307d9fbad6513cf0b3c6987
akmal81 <sah64078@gmail.com>
2025-05-26 09:19:00 +0600

    Update sql and clean up

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,54 +1,3 @@
-CREATE TABLE rangers (
-    ranger_id SERIAL PRIMARY KEY,
-    name VARCHAR(50),
-    region VARCHAR(50)
-);
-
-drop table rangers;
-
-CREATE TABLE species(
-    species_id SERIAL PRIMARY KEY,
-    common_name VARCHAR(50),
-    scientific_name VARCHAR(50),
-    discovery_date DATE,
-    conservation_status CHAR(10)
-);
-
-DELETE FROM species ;
-
-
-CREATE TABLE sightings(
-    sighting_id SERIAL PRIMARY KEY,
-    ranger_id INTEGER REFERENCES rangers(ranger_id) ON DELETE CASCADE,
-    species_id INTEGER REFERENCES species(species_id) ON DELETE CASCADE,
-    sighting_time TIMESTAMP WITHOUT TIME ZONE,
-    location VARCHAR(20),
-    notes TEXT
-);
-
-INSERT INTO rangers(name, region)
-VALUES ('Alice Green', 'Northern Hills'),
-  ('Bob White', 'River Delta'),
-  ('Carol King', 'Mountain Range');
-
-SELECT * from rangers;
-  INSERT INTO species_2 (common_name, scientific_name, discovery_date, conservation_status)
-VALUES
-  ('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
-  ('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
-  ('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
-  ('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
-SELECT * from species;
-  INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes)
-VALUES
-  (1, 1, 'Peak Ridge',        '2024-05-10 07:45:00+00', 'Camera trap image captured'),
-  (2, 2, 'Bankwood Area',     '2024-05-12 16:20:00+00', 'Juvenile seen'),
-  (3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00+00', 'Feeding observed'),
-  (1, 2, 'Snowfall Pass',     '2024-05-18 18:30:00+00', NULL);
-
-select * from  sightings;
-DROP Table sightings;
-
 
 -- 1  Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
 
@@ -76,9 +25,10 @@ SELECT common_name FROM species
 WHERE species_id NOT IN (SELECT species_id  from sightings );
 
 -- 6 Show the most recent 2 sightings.
+
 SELECT sp.common_name, s.sighting_time, r.name  FROM sightings s 
 JOIN species sp ON s.species_id =sp.species_id  
-JOIN rangers r on r.ranger_id = s.ranger_id
+JOIN rangers r ON r.ranger_id = s.ranger_id
 ORDER BY sighting_time DESC LIMIT 2;
 
 
@@ -102,6 +52,4 @@ FROM sightings;
 
 DELETE FROM rangers WHERE ranger_id NOT IN (SELECT ranger_id FROM sightings );
 
-INSERT into rangers (name, region)VALUES('Carol King', 'Mountain Range')
-
-SELECT * from rangers;
+-- end
